434ffd1288353796886a27281ab5e8d805cc0590
yanghaolei <yanghaolei@xiaohongshu.com>
2021-09-13 10:50:24 +0800

    first commit

---
+++ src/main/java/com/galaxy/droolspractice/DroolsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.galaxy.droolspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroolsPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/galaxy/droolspractice/DroolsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galaxy.droolspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
