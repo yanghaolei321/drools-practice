d0d735337c66737100cb0e6b7b39ff0fe3e21bb8
yanghaolei <yanghaolei@xiaohongshu.com>
2021-09-14 11:32:29 +0800

    java compile

---
+++ src/test/java/com/galaxy/droolspractice/compiler/bean/BeanProxy.java
@@ -0,0 +1,14 @@
+package com.galaxy.droolspractice.compiler.bean;
+
+/**
+ * Sample interface.
+ * 
+ * @author michael
+ */
+public interface BeanProxy {
+
+	void setDirty(boolean dirty);
+
+	boolean isDirty();
+
+}
---
+++ src/test/java/com/galaxy/droolspractice/compiler/bean/User.java
@@ -0,0 +1,17 @@
+package com.galaxy.droolspractice.compiler.bean;
+
+import lombok.Data;
+
+/**
+ * Sample class as JavaBean.
+ * 
+ * @author michael
+ */
+@Data
+public class User {
+
+	private String id;
+	private String name;
+	private long created;
+
+}
